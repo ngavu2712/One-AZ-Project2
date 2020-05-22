14fd83820ddf0b7b7e236ee9d428ec1868313847
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-05-21 19:16:47 -0700

    fixed api route from apiRoutes on server.js

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ var express = require("express");
 var app = express();
 // require("dotenv").config();
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3306;
 
 app.use(express.static("public"));
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-require("./routes/apiRoutes.js")(app);
+require("./routes/api-routes.js")(app);
 
-// Requiring our models for syncing
-var db = require("./models");
+
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(function() {
