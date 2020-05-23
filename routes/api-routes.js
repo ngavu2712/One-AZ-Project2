6a6b6550d2385886b2c2d7de40e5fa3c01b1f4e3
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-05-22 23:43:34 -0700

    Fixed api-routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,19 +7,31 @@ var Services = require("../models/services.js");
 //Routes
 //=========================================================
 module.exports = function(app) {
-//Get all orgs and services
-app.get("/api/all", function(req, res) {
-    Org.findAll({}).then(function(results) {
-      res.json(results);
+
+//Get all orgs
+app.get("/api/orgs", function(req, res) {
+    db.Org.findAll({}).then(function(dbOrg) {
+      res.json(dbOrg);
     });
   });
 
+  //Gets a specific org
+  app.get("/api/orgs/:id", function(req, res) {
+    // Find one Org with the id in req.params.id and return them to the user with res.json
+    db.Org.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbOrg) {
+      res.json(dbOrg);
+    });
+  });
 
   //create new org
   app.post("/api/org", function (req, res) {
     console.log("Create a new org");
 
-    db.azone.create({
+    db.Org.create({
         Name : req.body.Name,
         Email: req.body.Email,
         Phone: req.body.Phone,
@@ -32,17 +44,35 @@ app.get("/api/all", function(req, res) {
         Daycare: req.body.Daycare,
         Mental_Health: req.body.Mental_Health
 
-    }).then(function (result) {
+    }).then(function (Org) {
+       // send back the Org id to client
+       res.json(Org.id);
         console.log("Inserted a new organization into our org table!");
-    }).catch(function (err) {
+    }).catch(function(err){
+      //if error
         console.log(err);
+        //sends an error to the client
+        res.send(false);
     })
 })
 
 //Selecting a specific service
-app.get("/api/services/:serviceId", function(req, res) {
-    Org.findAll({}).then(function(results) {
-      res.json(results);
+app.get("/api/services/:id", function(req, res) {
+  // Find one Service with the id in req.params.id and return them to the user with res.json
+  db.Services.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(dbServices) {
+    res.json(dbServices);
+  });
+});
+
+
+  //Displays all services
+  app.get("/api/services", function(req, res) {
+    db.Services.findAll({}).then(function(dbServices) {
+      res.json(dbServices);
     });
   });
 
