6c9d1bdca15c930ccf247cd2615b2f2d250369df
Barry Green <Barry.Green@SLFHC.ORG>
2020-05-26 11:45:23 -0700

    added firebase configs

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,20 @@
+var firebaseConfig = { 
+    apiKey: "AIzaSyBwhvrEwCgQZX57BSmx9qJWYe4sqLRJ-hE", 
+    authDomain: "azone-86fa8.firebaseapp.com", 
+    databaseURL: "https://azone-86fa8.firebaseio.com", 
+    projectId: "azone-86fa8" 
+  }; 
+
+  // Initialize Firebase 
+  firebase.initializeApp(firebaseConfig); 
+
+
+  //make and store firestore references 
+  const auth = firebase.auth(); 
+  const db = firebase.firestore(); 
+  db.settings({timestampsInSnapshots: true}) 
+
+
 $("#add-btn").on("click", function(event){
     event.preventDefault(); 
     var orgInfo = {
